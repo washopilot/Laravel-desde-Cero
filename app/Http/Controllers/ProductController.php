347a706959fa1840e7803103784032bf0d1ee8a2
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-10 22:41:35 -0500

    implementing ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller {
+  public function index() {
+    return 'This is the list of products from CONTROLLER';
+  }
+
+  public function create() {
+    return 'This is the form to create a product from CONTROLLER';
+  }
+
+  public function store() {
+    //
+  }
+
+  public function show($product) {
+    return "Showing product with id {$product} from CONTROLLER";
+  }
+
+  public function edit($product) {
+    return "Showing the form to edit the product with id {$product} from CONTROLLER";
+  }
+
+  public function update($product) {
+    //
+  }
+
+  public function destroy($product) {
+    //
+  }
+}
