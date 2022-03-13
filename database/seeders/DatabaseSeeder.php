6ebe2e224732f5590da14fdd7b53f23a11a0ecfa
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-12 19:27:58 -0500

    create ProductFactory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+class DatabaseSeeder extends Seeder {
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run() {
+    $products = Product::factory()->count(20)->create();
+  }
 }
