0996c2da4b4a3a6198ceeb6a514f1b9ed0908085
Fernando Chicaiza <washopilot@yahoo.com>
2022-03-19 18:49:26 -0500

    delete products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -39,6 +39,8 @@ class ProductController extends Controller {
   }
 
   public function destroy($product) {
-    //
+    $product = Product::findOrFail($product);
+    $product->delete();
+    return $product;
   }
 }
