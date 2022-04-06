3977ced539abe77be31c9023a5e8f69de981886d
Fernando Chicaiza <washopilot@yahoo.com>
2022-04-06 09:51:51 -0500

    mostrando errores de validacion

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -15,6 +15,18 @@
       {{ session()->get('error') }}
     </div>
   @endif
+
+  @if (@isset($errors) && $errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+
+  @endif
+
   @yield('content')
 </body>
 
